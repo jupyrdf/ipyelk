d5fb97e9942a690e94165faa4bc15874ae1765f5
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2020-08-17 15:51:21 -0400

    move more things to setup.cfg

--- setup.py
+++ setup.py
@@ -7,37 +7,11 @@ from pathlib import Path
 import setuptools
 
 HERE = Path(__file__).parent
-name = "ipyelk"
-src = "py_src"
-
-setup_args = dict(
-    name=name,
-    description="ELK widget for Jupyter",
-    version=re.findall(
-        r'''__version__ = "([^"]+)"''',
-        (HERE / src / name / "_version.py").read_text(encoding="utf-8"),
-    )[0],
-    packages=setuptools.find_packages(src),
-    package_dir={name: f'{src}/{name}'},
-    author="Dane Freeman",
-    author_email="dane.freeman@gtri.gatech.edu",
-    url="https://github.edu/dfreeman06/ipyelk",
-    license="BSD-3-Clause",
-    platforms="Linux, Mac OS X, Windows",
-    keywords=["Jupyter", "Widgets", "IPython", "ElkJS"],
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Framework :: Jupyter",
-    ],
-    python_requires=">=3.7",
-    include_package_data=True,
-    install_requires=["ipywidgets>=7.5.0", "networkx"],
-)
 
 if __name__ == "__main__":
-    setuptools.setup(**setup_args)
+    setuptools.setup(
+        version=re.findall(
+            r'''__version__ = "([^"]+)"''',
+            (HERE / "py_src/ipyelk/_version.py").read_text(encoding="utf-8"),
+        )[0]
+    )
