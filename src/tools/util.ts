f208f13bdad245ed24db98aaa6b43baf2c146f59
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2020-10-22 10:52:16 -0400

    add copyright header

--- src/tools/util.ts
+++ src/tools/util.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2020 Dane Freeman.
+ * Distributed under the terms of the Modified BSD License.
+ */
+
 import { SModelElement } from 'sprotty/lib';
 
 export function idGetter(e: SModelElement) {
