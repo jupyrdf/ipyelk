6d0c3e886303f1455b96b4112ceb0832e717664f
Dane Freeman <dane.freeman@gtri.gatech.edu>
2021-05-16 16:13:13 -0400

    remove layout option widget examples

--- py_src/ipyelk/nx/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2021 Dane Freeman.
-# Distributed under the terms of the Modified BSD License.
-
-from .pipes import Diagram, NXSource
-
-__all__ = [
-    "Diagram",
-    "NXSource",
-]
