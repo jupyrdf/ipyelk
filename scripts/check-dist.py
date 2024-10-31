52e78f1a0031ad9e59c00c8a988113f7345c1a59
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2024-10-31 14:45:35 -0500

    yet more checks

--- scripts/check-dist.py
+++ scripts/check-dist.py
@@ -1,11 +1,11 @@
 """Check for files in dist archives."""
 
-from pathlib import Path
 import sys
-import zipfile
 import tarfile
-import tomllib
+import zipfile
+from pathlib import Path
 
+import tomllib
 
 HERE = Path(__file__).parent
 ROOT = HERE.parent
@@ -17,24 +17,27 @@ LICENSE = ROOT / "LICENSE.txt"
 COPYRIGHT = ROOT / "COPYRIGHT.md"
 EPL = ROOT / "third-party/epl-v10.html"
 TPL_PATH = (
-    "src/_d/share/jupyter/labextensions/@jupyrdf/jupyter-elk/static/"
-    "third-party-licenses.json"
+    "share/jupyter/labextensions/@jupyrdf/jupyter-elk/static/third-party-licenses.json"
 )
-TPL = ROOT / TPL_PATH
+TPL = ROOT / "src/_d" / TPL_PATH
 
 LICENSE_BYTES = {p: p.read_bytes() for p in [LICENSE, COPYRIGHT, EPL, TPL]}
 
 PY_VERSION = tomllib.loads(PPT.read_text(**UTF8))["project"]["version"]
+PFX = f"ipyelk-{PY_VERSION}"
 
 WHEEL_FILES = {
-    f"ipyelk-{PY_VERSION}.dist-info/LICENSE.txt": LICENSE_BYTES[LICENSE]
+    f"{PFX}.dist-info/LICENSE.txt": LICENSE_BYTES[LICENSE],
+    f"{PFX}.data/data/{TPL_PATH}": LICENSE_BYTES[TPL],
 }
+
 SDIST_FILES = {
-    f"ipyelk-{PY_VERSION}/LICENSE.txt": LICENSE_BYTES[LICENSE],
-    f"ipyelk-{PY_VERSION}/COPYRIGHT.md": LICENSE_BYTES[COPYRIGHT],
-    f"ipyelk-{PY_VERSION}/third-party/epl-v10.html": LICENSE_BYTES[EPL],
-    f"ipyelk-{PY_VERSION}/{TPL_PATH}": LICENSE_BYTES[TPL],
+    f"{PFX}/LICENSE.txt": LICENSE_BYTES[LICENSE],
+    f"{PFX}/COPYRIGHT.md": LICENSE_BYTES[COPYRIGHT],
+    f"{PFX}/third-party/epl-v10.html": LICENSE_BYTES[EPL],
+    f"{PFX}/src/_d/{TPL_PATH}": LICENSE_BYTES[TPL],
 }
+
 
 def check_whl(path: Path) -> None:
     with zipfile.ZipFile(path, "r") as whl:
@@ -42,11 +45,13 @@ def check_whl(path: Path) -> None:
             assert whl.read(fn) == fbytes, f"!!! wheel {fn} is wrong"
             print(f"OK wheel {fn}")
 
+
 def check_sdist(path: Path) -> None:
     with tarfile.open(path, "r:gz") as sdist:
         for fn, fbytes in SDIST_FILES.items():
             assert sdist.extractfile(fn).read() == fbytes, f"!!! sdist {fn} is wrong"
             print(f"OK sdist {fn}")
+
 
 def main() -> int:
     for path in sorted(DIST.glob("*")):
@@ -54,6 +59,7 @@ def main() -> int:
             check_whl(path)
         elif path.name.endswith(".tar.gz"):
             check_sdist(path)
+
 
 if __name__ == "__main__":
     sys.exit(main())
